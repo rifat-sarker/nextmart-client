c23a8a2a5c928795498fddd8d2d549ae7e62a2f1
rifat-sarker <rifatswd@gmail.com>
2025-02-18 13:21:06 +0600

    Creating a Context API to Access User Info in Client Side

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { getCurrentUser } from "@/services/AuthService";
+"use client";
+import { useUser } from "@/context/UserContext";
 
-export default async function HomePage() {
-  const user = await getCurrentUser()
+export default function HomePage() {
+  const user = useUser();
   console.log(user);
   return (
     <div>
       <h1>Welcome to nextmart home page</h1>
     </div>
   );
-};
+}
