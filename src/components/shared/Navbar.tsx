a1066e042233031b9c62ff3104adf58a0c4444b3
rifat-sarker <rifatswd@gmail.com>
2025-02-23 07:57:38 +0600

    Add Dashboard Layout with Sidebar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -14,13 +14,20 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { logout } from "@/services/AuthService";
 import { useUser } from "@/context/UserContext";
+import { usePathname, useRouter } from "next/navigation";
+import { protectedRoutes } from "@/constants";
 
 export default function Navbar() {
   const { user, setIsLoading } = useUser();
+  const pathname = usePathname();
+  const router = useRouter();
 
   const handleLogOut = () => {
     logout();
     setIsLoading(true);
+    if (protectedRoutes.some((route) => pathname.match(route))) {
+      router.push("/");
+    }
   };
   return (
     <header className="border-b w-full">
@@ -46,9 +53,7 @@ export default function Navbar() {
           {user ? (
             <>
               <Link href="/create-shop">
-                <Button className="rounded-full">
-                  Create shop
-                </Button>
+                <Button className="rounded-full">Create shop</Button>
               </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger>
