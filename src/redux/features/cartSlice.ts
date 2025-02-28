12cb52460ce904e3465c253a9c7adab04039fdae
rifat-sarker <rifatswd@gmail.com>
2025-02-28 15:10:49 +0600

    Updating Quantity and Removing Items

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -29,6 +29,29 @@ const cartSlice = createSlice({
 
       state.products.push({ ...action.payload, orderQuantity: 1 });
     },
+    incrementOrderQuantity: (state, action) => {
+      const productToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (productToIncrement && productToIncrement.orderQuantity > 1) {
+        productToIncrement.orderQuantity += 1;
+        return;
+      }
+    },
+    decrementOrderQuantity: (state, action) => {
+      const decrementToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (decrementToIncrement && decrementToIncrement.orderQuantity > 1) {
+        decrementToIncrement.orderQuantity -= 1;
+        return;
+      }
+    },
+    removeProduct: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
+    },
   },
 });
 
@@ -36,6 +59,11 @@ export const orderedProductsSelector = (state: RootState) => {
   return state.cart.products;
 };
 
-export const { addProduct } = cartSlice.actions;
+export const {
+  addProduct,
+  incrementOrderQuantity,
+  decrementOrderQuantity,
+  removeProduct,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
